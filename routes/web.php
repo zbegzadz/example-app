b9d58d7a3177ed9730c7660276eabc7a8b047cdd
zaki.begzad1 <zaki.begzad1@student.rocva.nl>
2025-09-14 14:00:21 +0200

    Fix routes/web.php changes

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $data = ["Uranus", "Jupiter", "Mars", "Aarde", "Saturnus", "Pluto", "Neptunus", "Venus"];
+    return view('planets', ['data' => $data]);
 });
