a5a62b35057ec289da3e68c8e046dda527342ad4
zaki.begzad1 <zaki.begzad1@student.rocva.nl>
2025-09-20 13:22:04 +0200

    opdracht views

--- resources/views/planets.blade.php
+++ resources/views/planets.blade.php
@@ -4,11 +4,19 @@
     <title>Planets</title>
 </head>
 <body>
-    <h1>Planets</h1>
+    <h1>Planet List</h1>
     <ul>
         @foreach($data as $planet)
             <li>{{ $planet }}</li>
         @endforeach
     </ul>
+
+    <h2>Planet Details</h2>
+    @foreach($planets as $planet)
+        <div style="margin-bottom: 20px;">
+            <h3>{{ $planet['name'] }}</h3>
+            <p>{{ $planet['description'] }}</p>
+        </div>
+    @endforeach
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,25 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     $data = ["Uranus", "Jupiter", "Mars", "Aarde", "Saturnus", "Pluto", "Neptunus", "Venus"];
-    return view('planets', ['data' => $data]);
+
+    $planets = [
+        [
+            'name' => 'Mars',
+            'description' => 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury.'
+        ],
+        [
+            'name' => 'Venus',
+            'description' => 'Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty.'
+        ],
+        [
+            'name' => 'Earth',
+            'description' => 'Our home planet is the third planet from the Sun, and the only place we know of so far thats inhabited by living things.'
+        ],
+        [
+            'name' => 'Jupiter',
+            'description' => 'Jupiter is a gas giant and doesn\'t have a solid surface, but it may have a solid inner core about the size of Earth.'
+        ],
+    ];
+
+    return view('planets', compact('data', 'planets'));
 });
