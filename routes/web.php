2d9851cff535c51e8b9a3423e93112d6b81fe24d
zaki.begzad1 <zaki.begzad1@student.rocva.nl>
2025-11-20 11:40:25 +0100

    Toegevoegd route voor homepage om PHPUnit-test te laten slagen

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PlanetController;
 
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/planets', [PlanetController::class, 'index']);
 Route::get('/planets/{planet}', [PlanetController::class, 'show']);
